ff3f9d099a746fe010a27a6be2f9b89bd8947497
Aqdas Ansari <113194530+Aqdas101@users.noreply.github.com>
2024-05-20 15:35:17 +0500

    Create pdf_to_image_conversion.py
    Extract all images from the PDF file

---
+++ pdf_to_image_conversion.py
@@ -0,0 +1,21 @@
+from typing import List
+from pdf2image import convert_from_path
+from PIL import Image
+from pydantic import FilePath, BaseModel
+
+class PDFToImageModel(BaseModel):
+    pdf_path: FilePath
+
+class pdf_to_image:
+  def __init__(self, pdf_path: str):
+    try:
+      validate_data = PDFToImageModel(pdf_path=pdf_path)
+    except Exception as e:
+      raise Exception ('Your Path Fromat Is Wrong')
+
+  def return_image_bytes(self) -> List[Image.Image]:
+    pages_images: List[Image.Image] = convert_from_path(self.pdf_path)
+    return pages_images
+
+  def save_image(self, name: str) -> None:
+    [i.save(f'{name}.jpg', 'JPEG') for i in self.return_image_bytes()]
